74cbe12bb7c11cfaaa9bf1993251b3e90b3fd01e
Mohsin Munir <mohsingoraya313@outlook.com>
2022-06-29 23:42:30 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,11 +10,14 @@ public class Main {
 //        int number1 = input.nextInt();
 //        show(number1);
 
-        System.out.println();
-        
+        System.out.print("Enter your Age");
+        int age = input.nextInt();
 
+        age = age * 12;
 
-
+        System.out.println("Your age in Months is = " + age);
+        System.out.println("Your age in days = " + age * 30);
+        System.out.println("Your age in hours is " + age * 24 );
     }
 
     public static void show(int num1){
