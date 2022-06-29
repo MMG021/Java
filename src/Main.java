d2adc4228152a3ae8234137d1dbaa211f8483070
Mohsin Munir <mohsingoraya313@outlook.com>
2022-06-29 20:34:12 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args ){
+
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a number");
+//        int number1 = input.nextInt();
+//        show(number1);
+
+        System.out.println();
+        
+
+
+
+    }
+
+    public static void show(int num1){
+        System.out.println("Your number is "+ num1);
+
+    }
+}
