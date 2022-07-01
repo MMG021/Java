b0bbeca8183ddaa721a60c16a9cba71e2f951987
Mohsin Munir <mohsingoraya313@outlook.com>
2022-07-01 14:30:20 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,32 @@ public class Main {
 //        int number1 = input.nextInt();
 //        show(number1);
 
-        System.out.print("Enter your Age");
+        System.out.print("Enter your Age ");
         int age = input.nextInt();
 
         age = age * 12;
 
-        System.out.println("Your age in Months is = " + age);
-        System.out.println("Your age in days = " + age * 30);
-        System.out.println("Your age in hours is " + age * 24 );
+        System.out.println("Your age in Months is = " + age );
+
+        System.out.println("Your age in days = " + age * 30 );
+
+        System.out.println("Your age in hours is " + age * 30 * 24 );
+
+        if (age <= 18) {
+            System.out.println("You are teenager");
+        }
+            else if (age >=23 & age <=40) {
+            System.out.println("You are Young");
+        }
+            else if ( age > 40 & age < 60 ) {
+            System.out.println("you are getting old");
+        }
     }
 
-    public static void show(int num1){
-        System.out.println("Your number is "+ num1);
 
-    }
+
+//    public static void show(int num1){
+//        System.out.println("Your number is "+ num1);
+//
+//    }
 }
